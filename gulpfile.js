622c77a6c74786ba3ff81baa3a9f7bbd58eeb23b
Alcina <white_kaitou@hotmail.com>
2017-10-09 21:48:21 -0400

    Basic set up

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+//var handlebars = require('handlebars');
+
+var browserSync = require('browser-sync').create();
+
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('hello', function() {
+  console.log('Hi!!');
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/css/*.css', browserSync.reload);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
+});
