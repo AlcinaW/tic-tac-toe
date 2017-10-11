cedf077e71132d2ab44782df368f63affca81b74
Alcina <white_kaitou@hotmail.com>
2017-10-10 23:39:41 -0400

    implementing humanPlayer checker

--- app/js/script.js
+++ app/js/script.js
@@ -23,7 +23,7 @@ function startGame() {
   //create array and give it a number
   originalBoard =  Array.from(Array(9).keys());
   console.log(originalBoard);
-  //change to map after?
+  //To DO: replaces this for loop?
   for (var i= 0; i < cells.length; i++) {
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
@@ -32,12 +32,53 @@ function startGame() {
 }
 
 function turnClick(square) {
+  //prevent clicking on spots already clicked
+
   //call turn function, pass in humanPlayer
   turn(square.target.id, humanPlayer);
-  console.log(square.target.id);
+  //console.log(square.target.id);
+  //before the AI plays, check if it is a tie
+  if(!checkTie()) turn(bestSpot(), aiPlayer);
 }
 
 function turn(squareId, player){
   originalBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  //check if player has on
+  //if won has been won, call gameOver
+  let gameWon = checkWin(originalBoard, player)
+  //pass into gameOver
+  //if gameWon, then gameOver
+  if (gameWon) gameOver(gameWon)
+}
+
+function checkWin (board, player) {
+  //pass in board because will be passing other versions of the board later one
+  //accumulator, initialize
+  //check the places on the board that have been played in
+  //reduce will go through the board array and do something
+  // e - element, i - index; take accumulator array and add the index of that array
+  //ternary operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+  let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  //check if game has been won
+  for (let [index, win] of winCombos.entries()) {
+    //for every element of winCombos, check if element is greater then -1
+    //has the player played in all these spots?
+    if (win.every(elem => plays.indexOf(elem) > -1)){
+      gameWon = {index: index, player: player};
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  //highlight winning combo, disable clicking
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = gameWon.player == humanPlayer ? "blue" : "red";
+  }
+  for (var i=0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
 }
